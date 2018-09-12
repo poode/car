08d57d8440c7eb3f86e3592d1ea650bc95e84f01
poode <eng.abdul.azeem@gmail.com>
2018-09-13 01:14:36 +0200

    fixing logging inside logger middleware

--- middleware/logHandler.js
+++ middleware/logHandler.js
@@ -1,10 +1,6 @@
 const { logger } = require('../config/logger');
-const loggerControl = require('../util/LoggersControl/loggerControl')();
 
 module.exports = (req, res, next) => {
-  if (!loggerControl) {
-    return next();
-  }
   logger.info(`method: ${req.method}, || url: ${req.url} || time: ${new Date()}`);
   return next();
 };
