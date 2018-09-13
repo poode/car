b32cb6a862a743486e9c1e3c0bb800528dbdbd33
poode <eng.abdul.azeem@gmail.com>
2018-09-13 14:28:12 +0200

    tring to make best practice in error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,33 +2,50 @@ const _ = require('lodash');
 const { logger } = require('../config/logger');
 
 process.on('unhandledRejection', (err) => {
-  throw err;
+  const error = {
+    time: new Date(),
+    type: 'unhandled promise',
+    message: err.message,
+    stack: err.stack,
+  };
+  throw error;
 });
 
 process.on('uncaughtException', (err) => {
-  const error = { message: err.message, stack: err.stack };
+  let error = {
+    time: new Date(),
+    type: 'expection',
+    message: err.message,
+    stack: err.stack,
+  };
+  if (err.type === 'unhandled promise') {
+    error = {
+      time: new Date(),
+      type: err.type,
+      message: err.message,
+      stack: err.stack,
+    };
+  }
   logger.error(JSON.stringify(error));
   process.exit();
 });
 
-/*eslint-disable */ // next is a part of middleware method signature I cannot ignore.
 module.exports = (err, req, res, next) => {
-  /*eslint-disable */ // I know that no way to attach property but I will use. 
-  if (!err.status) err.status = 500;
-  if (err.name === 'TokenExpiredError') {
-    err.status = 403;
-    err.message = `your session has been expired at ${err.expiredAt}`;
-  }
-  /* eslint-enable */
+  // if (err.name === 'TokenExpiredError') {
+  //   err = {
+  //     status: 403,
+  //     message: `your session has been expired at ${err.expiredAt}`,
+  //     stack: '',
+  //   };
+  // }
   const error = {
-    message: err.message,
+    message: err.message || err.name,
+    stack: err.stack,
     requestedUrl: req.url,
     requestedMethod: req.method,
     requestTime: `${new Date()}`,
-    status: err.status,
-    stack: err.stack,
+    status: err.status || 500,
   };
-  /* eslint-enable */
   logger.error(JSON.stringify(error));
   return res.status(err.status).json({ error: _.pick(error, ['message', 'status']) });
 };
