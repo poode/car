0b99cb5ba1a770a070ab67747459d5c41eb4b20d
poode <eng.abdul.azeem@gmail.com>
2018-09-15 16:31:09 +0200

    changing DB configuration to match sequelize

--- config/db.js
+++ config/db.js
@@ -1,29 +1,41 @@
-const mysql = require('mysql2/promise');
+const Sequelize = require('sequelize');
 
 const { logger } = require('./logger');
 
-async function db() {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    });
-    logger.info('connected to database...');
-    return connection;
-  } catch (error) {
-    logger.error(`unable to connect to database... error: ${JSON.stringify(error)}`);
-    return false;
-  }
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_TYPE,
+    operatorsAliases: false,
 
-module.exports = (async () => {
-  try {
-    const connection = await db();
-    return connection;
-  } catch (error) {
-    return error;
-  }
-})();
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    define: {
+      underscored: false,
+      freezeTableName: false,
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci',
+      },
+      timestamps: true,
+    },
+  },
+);
+
+sequelize.authenticate()
+  .then(() => {
+    logger.info('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    logger.error(`Unable to connect to the database:${JSON.stringify(err)}`);
+  });
+
+
+module.exports = {
+  db: sequelize,
+};
