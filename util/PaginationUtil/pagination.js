da2f1716f1945f517e40a2bd87897eaaaf9781fb
poode <eng.abdul.azeem@gmail.com>
2018-09-16 00:58:46 +0200

    adding pagination based on sequelize

--- util/PaginationUtil/pagination.js
+++ util/PaginationUtil/pagination.js
@@ -1,16 +1,16 @@
 const paginate = require('express-paginate');
 
 async function pagination(model, req) {
-  const [results, itemCount] = await Promise.all([
-    model.find({}).limit(req.query.limit).skip(req.skip).lean()
-      .exec(),
-    model.count({}),
-  ]);
+  const results = await model.findAndCountAll({
+    limit: req.query.limit,
+    offset: req.skip,
+  });
+  const itemCount = results.count;
   const pageCount = Math.ceil(itemCount / req.query.limit);
   return {
     object: 'list',
     has_more: paginate.hasNextPages(req)(pageCount),
-    data: results,
+    data: results.rows,
     pageCount,
     itemCount,
     pages: paginate.getArrayPages(req)(3, pageCount, req.query.page),
