dce2c47774c4f1773d8b9662b94a48ca117c2934
poode <eng.abdul.azeem@gmail.com>
2018-09-16 00:59:59 +0200

    making global validation method will be used to validate based on provided schema and user input schema

--- util/helpers/validation.js
+++ util/helpers/validation.js
@@ -1,7 +1,22 @@
 const Ajv = require('ajv');
+const localize = require('ajv-i18n');
 
-module.exports = function validate(schema, data) {
-  const ajv = new Ajv();
-  const valid = ajv.validate(schema, data);
-  return valid;
+const ajv = new Ajv({ allErrors: true });
+require('ajv-keywords')(ajv, 'uniqueItemProperties');
+
+module.exports = function validate(modelSchema, inputSchema) {
+  const validateSchema = ajv.compile(modelSchema);
+  const valid = validateSchema(inputSchema);
+  const errorArray = [];
+  if (!valid) {
+    // localize.ar(validateSchema.errors);
+    validateSchema.errors.map((err) => {
+      if (!err.dataPath) {
+        errorArray.push({ fieldRequirments: `${JSON.stringify(err.params)} ${err.message}` });
+      } else {
+        errorArray.push({ fieldRequirments: `${err.dataPath} ${err.message}` });
+      }
+    });
+  }
+  return errorArray;
 };
