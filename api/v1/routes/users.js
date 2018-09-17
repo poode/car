71702c496c9375bdd77328505a7aa785ff7a5c4b
poode <eng.abdul.azeem@gmail.com>
2018-09-17 12:29:49 +0200

    open only register API for user creation

--- api/v1/routes/users.js
+++ api/v1/routes/users.js
@@ -1,23 +1,25 @@
 const router = require('express-promise-router')();
+const { isAuthorized } = require('../../../middleware/authorization');
+
 
 const UserController = require('../controllers/UserController');
 
-router.get('/', UserController.index.bind(UserController));
+router.get('/', isAuthorized, UserController.index.bind(UserController));
 
-router.get('/usersList', UserController.getLimited.bind(UserController));
+router.get('/usersList', isAuthorized, UserController.getLimited.bind(UserController));
 
-router.get('/mobile/:mobile', UserController.getUser.bind(UserController));
+router.get('/mobile/:mobile', isAuthorized, UserController.getUser.bind(UserController));
 
-router.get('/id/:id', UserController.getUser.bind(UserController));
+router.get('/id/:id', isAuthorized, UserController.getUser.bind(UserController));
 
 router.post('/register', UserController.create.bind(UserController));
 
-router.delete('/delete/id/:id', UserController.deleteUser.bind(UserController));
+router.delete('/delete/id/:id', isAuthorized, UserController.deleteUser.bind(UserController));
 
-router.delete('/delete/mobile/:mobile', UserController.deleteUser.bind(UserController));
+router.delete('/delete/mobile/:mobile', isAuthorized, UserController.deleteUser.bind(UserController));
 
-// router.put('/:id', UserController.update.bind(UserController));
+// router.put('/id/:id', UserController.update.bind(UserController));
 
-// router.delete('/:id', UserController.destroy.bind(UserController));
+// router.put('/mobile/:mobile', UserController.update.bind(UserController));
 
 module.exports = router;
