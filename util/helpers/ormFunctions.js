187bfd8031ae36862f580e4cbc188c123f29f8dd
poode <eng.abdul.azeem@gmail.com>
2018-09-17 11:49:47 +0200

    linting

--- util/helpers/ormFunctions.js
+++ util/helpers/ormFunctions.js
@@ -1,7 +1,8 @@
 /**
  * @param {*} model object
  * @param {*} options options should be sequelize option object for findOrCreate method
- * @returns {*} modelInstance which is created object if it does not exist or found an instance of it
+ * @returns {*} modelInstance which is created object
+ * if it does not exist or found an instance of it
  * modelExists which is boolean if true then modelInstance is created
  */
 async function findOrCreate(model, options) {
