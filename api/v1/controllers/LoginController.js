18c95efc9e92d9fabff299bccb7bc46383c8a3d6
poode <eng.abdul.azeem@gmail.com>
2018-09-18 17:00:21 +0200

    handle login error for wrong mobile length

--- api/v1/controllers/LoginController.js
+++ api/v1/controllers/LoginController.js
@@ -2,7 +2,7 @@ const { authentication } = require('../../../services/login.service');
 
 async function index(req, res, next) {
   const { token, error, user } = await authentication(req.body);
-  if (error) return next(error);
+  if (error) return next({ message: error, status: 422 });
   return res.header('x-auth-token', token).send({
     authenticated: true,
     user,
