de45a0e8aa91649573f1e540fe9abc7e9cdd3006
poode <eng.abdul.azeem@gmail.com>
2018-09-19 18:00:40 +0200

    adding virification middleware

--- api/v1/routes/users.js
+++ api/v1/routes/users.js
@@ -1,22 +1,22 @@
 const router = require('express-promise-router')();
 const { isAuthorized } = require('../../../middleware/authorization');
-
+const { verificationMiddleware } = require('../../../middleware/userMiddlewares/verifyUser');
 
 const UserController = require('../controllers/UserController');
 
-router.get('/', isAuthorized, UserController.index.bind(UserController));
+router.get('/', isAuthorized, verificationMiddleware, UserController.index.bind(UserController));
 
-router.get('/limit/:limit/page/:page', isAuthorized, UserController.getLimited.bind(UserController));
+router.get('/limit/:limit/page/:page', isAuthorized, verificationMiddleware, UserController.getLimited.bind(UserController));
 
-router.get('/mobile/:mobile', isAuthorized, UserController.getUser.bind(UserController));
+router.get('/mobile/:mobile', isAuthorized, verificationMiddleware, UserController.getUser.bind(UserController));
 
-router.get('/id/:id', isAuthorized, UserController.getUser.bind(UserController));
+router.get('/id/:id', isAuthorized, verificationMiddleware, UserController.getUser.bind(UserController));
 
 router.post('/register', UserController.create.bind(UserController));
 
-router.delete('/delete/id/:id', isAuthorized, UserController.deleteUser.bind(UserController));
+router.delete('/delete/id/:id', isAuthorized, verificationMiddleware, UserController.deleteUser.bind(UserController));
 
-router.delete('/delete/mobile/:mobile', isAuthorized, UserController.deleteUser.bind(UserController));
+router.delete('/delete/mobile/:mobile', isAuthorized, verificationMiddleware, UserController.deleteUser.bind(UserController));
 
 // router.put('/id/:id', UserController.update.bind(UserController));
 
