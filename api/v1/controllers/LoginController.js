571a71a4f9d66a7dd9da24a1878b274760164ca6
poode <eng.abdul.azeem@gmail.com>
2018-09-30 09:39:58 +0200

    remove unneeded require

--- api/v1/controllers/LoginController.js
+++ api/v1/controllers/LoginController.js
@@ -1,5 +1,4 @@
 const { signIn } = require('../../../services/login.service');
-const { logger } = require('../../../config/logger');
 
 async function index(req, res, next) {
   const { token, error, user } = await signIn(req.body, res);
