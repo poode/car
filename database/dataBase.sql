949c0b95dcfde27bcd89742904ac2b5184198812
poode <eng.abdul.azeem@gmail.com>
2018-09-30 11:03:51 +0200

    removed undscored cases and edited estimated time in LTService table as time

--- database/dataBase.sql
+++ database/dataBase.sql
@@ -5,13 +5,13 @@ CREATE TABLE `users` (
 	`password` VARCHAR(255) NOT NULL,
 	`mobile` VARCHAR(255) NOT NULL UNIQUE,
 	`verified` BOOLEAN NOT NULL DEFAULT false,
-	`verification` INT NOT NULL DEFAULT 0,
+	`verification` INT NOT NULL DEFAULT '0',
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`,`mobile`)
 );
 
-CREATE TABLE `LT_carTypes` (
+CREATE TABLE `LTCarTypes` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`carType` VARCHAR(255) NOT NULL UNIQUE,
 	`createdAt` BINARY,
@@ -19,11 +19,11 @@ CREATE TABLE `LT_carTypes` (
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `LT_serviceTypes` (
+CREATE TABLE `LTServiceTypes` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`serviceType` VARCHAR(255) NOT NULL UNIQUE,
 	`servicePrice` INT NOT NULL,
-	`estimatedTime` DATETIME NOT NULL,
+	`estimatedTime` Time NOT NULL,
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`)
@@ -31,16 +31,16 @@ CREATE TABLE `LT_serviceTypes` (
 
 CREATE TABLE `contactUs` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`contactUsReasonType_id` INT NOT NULL,
+	`contactUsReasonTypeId` INT NOT NULL,
 	`body` TEXT NOT NULL,
 	`imageOrVideo` VARCHAR(255),
-	`user_id` INT NOT NULL,
+	`userId` INT NOT NULL,
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `LT_contactUsReasonTypes` (
+CREATE TABLE `LTContactUsReasonTypes` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`reasonType` VARCHAR(255) NOT NULL UNIQUE,
 	`createdAt` DATETIME,
@@ -50,21 +50,21 @@ CREATE TABLE `LT_contactUsReasonTypes` (
 
 CREATE TABLE `orders` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`user_id` INT NOT NULL,
-	`carType_id` INT NOT NULL,
-	`serviceType_id` INT NOT NULL,
+	`userId` INT NOT NULL,
+	`carTypeId` INT NOT NULL,
+	`serviceTypeId` INT NOT NULL,
 	`location` VARCHAR(255) NOT NULL,
 	`startTime` DATETIME,
 	`arrivingTime` DATETIME,
 	`endTime` DATETIME,
-	`employee_id` INT,
-	`endOrderReasonType_id` INT,
+	`employeeId` INT,
+	`endOrderReasonTypeId` INT,
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `LT_endOrderReasonTypes` (
+CREATE TABLE `LTEndOrderReasonTypes` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`endOrderReason` VARCHAR(255) NOT NULL,
 	`createdAt` DATETIME,
@@ -74,7 +74,7 @@ CREATE TABLE `LT_endOrderReasonTypes` (
 
 CREATE TABLE `employeeAttendance` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`employee_id` INT NOT NULL,
+	`employeeId` INT NOT NULL,
 	`attendanceTime` DATETIME NOT NULL,
 	`LeavingTime` DATETIME NOT NULL,
 	`createdAt` DATETIME,
@@ -84,14 +84,14 @@ CREATE TABLE `employeeAttendance` (
 
 CREATE TABLE `evaluations` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`order_id` INT NOT NULL,
+	`orderId` INT NOT NULL,
 	`stars` INT NOT NULL DEFAULT '1',
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `LT_permissionTypes` (
+CREATE TABLE `LTPermissionTypes` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`permissionType` VARCHAR(255) NOT NULL UNIQUE,
 	`createdAt` DATETIME,
@@ -101,14 +101,14 @@ CREATE TABLE `LT_permissionTypes` (
 
 CREATE TABLE `userRoles` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`user_id` INT NOT NULL,
-	`roleType_id` INT NOT NULL,
+	`userId` INT NOT NULL,
+	`roleTypeId` INT NOT NULL,
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `LT_roleTypes` (
+CREATE TABLE `LTRoleTypes` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`roleType` VARCHAR(255) NOT NULL UNIQUE,
 	`createdAt` DATETIME,
@@ -118,35 +118,35 @@ CREATE TABLE `LT_roleTypes` (
 
 CREATE TABLE `rolePermissions` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`roleType_id` INT NOT NULL,
-	`permission_id` INT NOT NULL,
+	`roleTypeId` INT NOT NULL,
+	`permissionId` INT NOT NULL,
 	`createdAt` DATETIME,
 	`updatedAt` DATETIME,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `contactUs` ADD CONSTRAINT `contactUs_fk0` FOREIGN KEY (`contactUsReasonType_id`) REFERENCES `LT_contactUsReasonTypes`(`id`);
+ALTER TABLE `contactUs` ADD CONSTRAINT `contactUs_fk0` FOREIGN KEY (`contactUsReasonTypeId`) REFERENCES `LTContactUsReasonTypes`(`id`);
 
-ALTER TABLE `contactUs` ADD CONSTRAINT `contactUs_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE `contactUs` ADD CONSTRAINT `contactUs_fk1` FOREIGN KEY (`userId`) REFERENCES `users`(`id`);
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`userId`) REFERENCES `users`(`id`);
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`carType_id`) REFERENCES `LT_carTypes`(`id`);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`carTypeId`) REFERENCES `LTCarTypes`(`id`);
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`serviceType_id`) REFERENCES `LT_serviceTypes`(`id`);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`serviceTypeId`) REFERENCES `LTServiceTypes`(`id`);
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_fk3` FOREIGN KEY (`employee_id`) REFERENCES `users`(`id`);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk3` FOREIGN KEY (`employeeId`) REFERENCES `users`(`id`);
 
-ALTER TABLE `orders` ADD CONSTRAINT `orders_fk4` FOREIGN KEY (`endOrderReasonType_id`) REFERENCES `LT_endOrderReasonTypes`(`id`);
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk4` FOREIGN KEY (`endOrderReasonTypeId`) REFERENCES `LTEndOrderReasonTypes`(`id`);
 
-ALTER TABLE `employeeAttendance` ADD CONSTRAINT `employeeAttendance_fk0` FOREIGN KEY (`employee_id`) REFERENCES `users`(`id`);
+ALTER TABLE `employeeAttendance` ADD CONSTRAINT `employeeAttendance_fk0` FOREIGN KEY (`employeeId`) REFERENCES `users`(`id`);
 
-ALTER TABLE `evaluations` ADD CONSTRAINT `evaluations_fk0` FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`);
+ALTER TABLE `evaluations` ADD CONSTRAINT `evaluations_fk0` FOREIGN KEY (`orderId`) REFERENCES `orders`(`id`);
 
-ALTER TABLE `userRoles` ADD CONSTRAINT `userRoles_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE `userRoles` ADD CONSTRAINT `userRoles_fk0` FOREIGN KEY (`userId`) REFERENCES `users`(`id`);
 
-ALTER TABLE `userRoles` ADD CONSTRAINT `userRoles_fk1` FOREIGN KEY (`roleType_id`) REFERENCES `LT_roleTypes`(`id`);
+ALTER TABLE `userRoles` ADD CONSTRAINT `userRoles_fk1` FOREIGN KEY (`roleTypeId`) REFERENCES `LTRoleTypes`(`id`);
 
-ALTER TABLE `rolePermissions` ADD CONSTRAINT `rolePermissions_fk0` FOREIGN KEY (`roleType_id`) REFERENCES `LT_roleTypes`(`id`);
+ALTER TABLE `rolePermissions` ADD CONSTRAINT `rolePermissions_fk0` FOREIGN KEY (`roleTypeId`) REFERENCES `LTRoleTypes`(`id`);
 
-ALTER TABLE `rolePermissions` ADD CONSTRAINT `rolePermissions_fk1` FOREIGN KEY (`permission_id`) REFERENCES `LT_permissionTypes`(`id`);
+ALTER TABLE `rolePermissions` ADD CONSTRAINT `rolePermissions_fk1` FOREIGN KEY (`permissionId`) REFERENCES `LTPermissionTypes`(`id`);
