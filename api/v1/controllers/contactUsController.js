a6364be432e29b16183265be03e9d5d3245d3d76
poode <eng.abdul.azeem@gmail.com>
2018-10-02 16:55:14 +0200

    handling form with uploading Photo or video in a once

--- api/v1/controllers/contactUsController.js
+++ api/v1/controllers/contactUsController.js
@@ -1,72 +1,99 @@
 const path = require('path');
 
 const { contactUs, user, contactUsReason } = require('../../../config/db').db;
+const { limitedContactUs } = require('../../../services/contactUs.service.js');
+const { validate } = require('../../../util/helpers/validation');
+const contactUsSchema = require('../schema/contactUsSchema.json');
+const { pagination } = require('../../../util/PaginationUtil/pagination');
+
 
 async function index(req, res) {
   // slow query as it takes 1 to 2 sec.
-  // const contactUsShow = await contactUs.findAll({
-  //   include: [
-  //     {
-  //       model: user,
-  //       attributes: ['username', 'mobile'],
-  //     },
-  //     {
-  //       model: contactUsReason,
-  //       attributes: ['reasonType'],
-  //     },
-  //   ],
-  //   attributes: ['body', 'imageOrVideo'],
-  // });
+  const contactUsShow = await contactUs.findAll({
+    include: [
+      {
+        model: user,
+        attributes: ['username', 'mobile'],
+      },
+      {
+        model: contactUsReason,
+        attributes: ['reasonType'],
+      },
+    ],
+    attributes: ['body', 'imageOrVideoPath'],
+  });
 
   // faster query
-  const contactUsShow = [];
-  await contactUs.findAll().map(async (oneContactUs) => {
-    const userFound = await user.find({
-      where: { id: oneContactUs.userId },
-      attributes: ['username', 'mobile'],
-    });
-    const contactReason = await contactUsReason.find({
-      where: { id: oneContactUs.contactUsReasonTypeId },
-      attributes: ['reasonType'],
-    });
-    contactUsShow.push({
-      username: userFound.username,
-      mobile: userFound.mobile,
-      ContactUsReason: contactReason.reasonType,
-      body: oneContactUs.body,
-    });
-  });
+  // const contactUsShow = [];
+  // await contactUs.findAll().map(async (oneContactUs) => {
+  //   const userFound = await user.find({
+  //     where: { id: oneContactUs.userId },
+  //     attributes: ['username', 'mobile'],
+  //   });
+  //   const contactReason = await contactUsReason.find({
+  //     where: { id: oneContactUs.contactUsReasonTypeId },
+  //     attributes: ['reasonType'],
+  //   });
+  //   contactUsShow.push({
+  //     username: userFound.username,
+  //     mobile: userFound.mobile,
+  //     ContactUsReason: contactReason.reasonType,
+  //     body: oneContactUs.body,
+  //     filePath: oneContactUs.imageOrVideoPath,
+  //   });
+  // });
 
   res.json(contactUsShow);
 }
 
-// this function will use form-data header not raw header as no json will be posted
-async function uploadPhoto(req, res, next) {
-  const file = req.files;
-  const imageData = {
-    originalName: file.originalname,
-    mimeType: file.mimetype,
-    path: `http://${req.hostname}:${process.env.PORT}/static/${file.filename}`,
-    filename: file.filename,
-    size: file.size,
+async function postContactUs(req, res, next) {
+  // convert IDs to be numbers as I configured them to be numbers in here and Database
+  req.body.contactUsReasonTypeId = Number(req.body.contactUsReasonTypeId);
+  req.body.userId = Number(req.body.userId);
+
+  const errors = validate(contactUsSchema, req.body);
+  if (errors.length) return next({ message: errors, status: 400 });
+
+  const createdContactUs = await contactUs.create({
+    contactUsReasonTypeId: Number(req.body.contactUsReasonTypeId),
+    body: req.body.body,
+    imageOrVideoPath: req.file ? req.file.path : null,
+    userId: Number(req.body.userId),
+  });
+
+  const contactUsResponse = {
+    data: createdContactUs,
+    file: req.file ? req.file : null,
   };
-  res.json(imageData);
+
+  return res.json(contactUsResponse);
 }
 
-async function postContactUs(req, res, next) {
-  // @TODO validation
-  // @TODO return result or error from uploading image
-  const createdContactUs = await contactUs.create({
-    contactUsReasonTypeId: req.body.contactUsReasonTypeId,
-    body: req.body.body,
-    imageOrVideo: req.file ? req.file.path : null,
-    userId: req.body.userId,
-  });
-  return res.json(createdContactUs);
+// this function will use form-data header not raw header as no json will be posted
+async function uploadPhoto(req, res, next) {
+  const { file } = req;
+  if (file) {
+    const uploadedFile = {
+      originalName: file.originalname,
+      mimeType: file.mimetype,
+      path: `/static/${file.filename}`,
+      filename: file.filename,
+      size: file.size,
+    };
+    req.file = uploadedFile;
+  }
+  return postContactUs(req, res, next);
+}
+
+async function contactUsLimited(req, res, next) {
+  const { result, error } = await limitedContactUs(contactUs, req);
+  if (error) return next(error);
+  return res.json(result);
 }
 
 module.exports = {
   index,
   postContactUs,
   uploadPhoto,
+  contactUsLimited,
 };
