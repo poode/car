d4a7d20967960fbbfba5fb951fe741b1bcc0e44c
poode <eng.abdul.azeem@gmail.com>
2018-10-08 00:38:43 +0200

    handle validation errors

--- api/v1/controllers/orderController.js
+++ api/v1/controllers/orderController.js
@@ -2,6 +2,7 @@ const { setOrder } = require('../../../services/order.service');
 
 async function create(req, res, next) {
   const { orderFound, error } = await setOrder(req, res);
+  if (error) return next(error);
   return res.json(orderFound);
 }
 
--- services/order.service.js
+++ services/order.service.js
@@ -1,33 +1,46 @@
 const {
   orders,
 } = require('../config/db').db;
-
-const { findOrCreate } = require('../util/helpers/ormFunctions');
+const { validate } = require('../util/helpers/validation');
+const orderSchema = require('../api/v1/schema/orderSchema.json');
 
 async function setOrder(req, res) {
   const results = {
     error: '',
     orderFound: '',
   };
-  const { carTypeId, serviceTypeId, location } = req.body;
-
-  const options = {
-    where: { },
-    default: {
-      userId: res.locals.userId,
-      carTypeId,
-      serviceTypeId,
-      location,
-    },
-  };
-  const { modelInstance, modelExists } = await findOrCreate(orders, options);
-
-  if (modelExists === false) {
-    results.error = { message: 'Order already requested!', status: 409 };
+  const errors = validate(orderSchema, req.body);
+  if (errors.length) {
+    results.error = { message: errors, status: 400 };
     return results;
   }
+  const { carTypeId, serviceTypeId, location } = req.body;
 
-  results.orderFound = modelInstance;
+  const findPreviousOrder = await orders.findOne({
+    where: { userId: res.locals.userId },
+    order: [['id', 'DESC']],
+  });
+
+
+  if (findPreviousOrder) {
+    const orderDay = Number(JSON.stringify(new Date(`${findPreviousOrder.startTime}UTC`)).split('-')[2].split('T')[0]);
+    const today = new Date().getDate();
+    if (orderDay === today) {
+      results.error = { message: 'You asked today for a service. Ask again tomorrow', status: 412 };
+      return results;
+    }
+  }
+
+  const options = {
+    userId: Number(res.locals.userId),
+    carTypeId: Number(carTypeId),
+    serviceTypeId: Number(serviceTypeId),
+    location,
+  };
+
+  const order = await orders.create(options);
+
+  results.orderFound = order;
   return results;
 }
 
