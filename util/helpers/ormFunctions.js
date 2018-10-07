809a430795d5d5423effc670f2739d01797c7a14
poode <eng.abdul.azeem@gmail.com>
2018-10-07 23:52:20 +0200

    destruct array

--- util/helpers/ormFunctions.js
+++ util/helpers/ormFunctions.js
@@ -1,17 +1,18 @@
 /**
- * @param {*} model object
+ * @param {*} model sequelize model object
  * @param {*} options options should be sequelize option object for findOrCreate method
  * @returns {*} modelInstance which is created object
  * if it does not exist or found an instance of it
  * modelExists which is boolean if true then modelInstance is created
  */
 async function findOrCreate(model, options) {
+  if (!options.where || !options.defaults) throw Error('options should have where and defaults');
   const any = await model.findOrCreate({
     where: options.where,
     defaults: options.defaults,
   });
-  // @TODO array Destruction instead of any[]
-  return { modelInstance: any[0], modelExists: any[1] };
+  const [modelInstance, modelExists] = any;
+  return { modelInstance, modelExists };
 }
 
 module.exports = {
